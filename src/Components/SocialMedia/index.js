f48be7267efd7589d2cbecdd12dfdc8f7e3c591d
sayedmuhammed74 <73748545+sayedmuhammed74@users.noreply.github.com>
2022-03-06 16:47:31 +0200

    Update index.js

--- src/Components/SocialMedia/index.js
+++ src/Components/SocialMedia/index.js
@@ -1,16 +1,20 @@
 import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+// import axios from 'axios';
+import data from './../../../src/data.json';
+
 import {SocialSection, Social, SocialDesc, Span, SpanInfo, Icon} from './style.js';
 
 const SocialMedia = ()  =>{
 
-    const [social, setSocial] = useState([])
+    // const [social, setSocial] = useState([])
 
-    useEffect( () => {
-      axios.get('js/data.json').then(res => {setSocial(res.data.social)})
-    }, [])
+    // useEffect( () => {
+    //   axios.get('js/data.json').then(res => {setSocial(res.data.social)})
+    // }, [])
 
-    const socialList = social.map(socialItem => {
+    console.log(data.social);
+
+    const socialList = data.social.map(socialItem => {
 
       return (
         <Social item={socialItem.id} key={socialItem.id}>
